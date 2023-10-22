87ef7906905802ffc991dae762e4cde08a5bef96
Andriy Kobetiak <andriykobetiak@gmail.com>
2023-10-22 18:21:03 +0300

    optimize-performance

--- lib/src/component/todos/TodoItem/todo_item.dart
+++ lib/src/component/todos/TodoItem/todo_item.dart
@@ -49,7 +49,6 @@ class _TodoItemState extends State<TodoItem> {
   Widget build(BuildContext context) {
     var isDone = widget.item.isDone;
     var title = widget.item.title;
-    var onDelete = widget.onDelete;
 
     return Container(
       // add top border
@@ -72,62 +71,55 @@ class _TodoItemState extends State<TodoItem> {
             isHovered = false;
           });
         },
-        child: GestureDetector(
-          onDoubleTap:handleEditRequest,
-          onLongPress: handleEditRequest,
-          child: ListTile(
-            leading: Transform.scale(
-              scale: 1.25,
-              child: Checkbox(
-                fillColor: MaterialStateProperty.all(
-                    Colors.transparent,
-                ),
-                splashRadius: 14,
-                activeColor: Colors.transparent,
-                checkColor: Colors.green,
-                value: isDone,
-                onChanged: widget.onStatusChange,
-                shape: const CircleBorder(),
-                side: const BorderSide(
-                  width: 1,
-                  color: Color.fromRGBO(230, 230, 230, 1),
-                ),
+        child: ListTile(
+          leading: Transform.scale(
+            scale: 1.25,
+            child: Checkbox(
+              fillColor: MaterialStateProperty.all(
+                  Colors.transparent,
+              ),
+              splashRadius: 14,
+              activeColor: Colors.transparent,
+              checkColor: Colors.green,
+              value: isDone,
+              onChanged: widget.onStatusChange,
+              shape: const CircleBorder(),
+              side: const BorderSide(
+                width: 1,
+                color: Color.fromRGBO(230, 230, 230, 1),
               ),
             ),
-            trailing: isHovered
-                ? IconButton(
-                    icon: const Icon(Icons.close),
-                    onPressed: onDelete,
-                  )
-                : null,
-            title: isEditing
-              ? TextField(
-                focusNode: _focusNode,
-                controller: _controller,
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    newTitle = value;
-                  });
-                },
-                onTapOutside: (_) {
-                  onEditingComplete();
-                },
-                onEditingComplete: onEditingComplete,
-                onSubmitted: (_) {
-                  onEditingComplete();
-                },
-              )
-              : Text(
-                title,
-                style: TextStyle(
-                  decoration: isDone
-                    ? TextDecoration.lineThrough
-                    : TextDecoration.none,
-                ),
+          ),
+          trailing: isHovered
+              ? IconButton(
+                  icon: const Icon(Icons.close),
+                  onPressed: widget.onDelete,
+                )
+              : null,
+          title: isEditing
+            ? TextField(
+              focusNode: _focusNode,
+              controller: _controller,
+              decoration: const InputDecoration(
+                border: InputBorder.none,
               ),
+              onChanged: (value) {
+                setState(() {
+                  newTitle = value;
+                });
+              },
+              onTapOutside: (_) {
+                onEditingComplete();
+              },
+              onEditingComplete: onEditingComplete,
+              onSubmitted: (_) {
+                onEditingComplete();
+              },
+            )
+            : GestureDetector(
+              onDoubleTap:handleEditRequest,
+              onLongPress: handleEditRequest,
+              child: Text(title),
           ),
         ),
       ),
